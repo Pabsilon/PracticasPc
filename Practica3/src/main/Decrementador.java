05419d6d942b61b478cc1c88dd16298f44f6b823
Pabs <pablomacveigh@gmail.com>
2016-06-04 19:14:06 +0200

    Practica 3

---
+++ Practica3/src/main/Decrementador.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.concurrent.Semaphore;
+
+public class Decrementador extends Thread{
+	
+	Semaphore _sem;
+	int _n;
+	
+	public Decrementador (int n, Semaphore sem){
+		_n = n;
+		_sem = sem;
+	}
+	
+	public void run(){
+		
+		for (int i = 0; i<_n; i++){
+			try {
+				_sem.acquire();;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Problema1._var = Problema1._var -i;
+			_sem.release();
+		}
+		
+	}
+
+}
---
+++ Practica3/src/main/Problema1.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.concurrent.Semaphore;
+
+public class Problema1 {
+
+	Thread[] _threads;
+	int _n;
+	static int _var;
+	Semaphore _sem;
+	
+	public Problema1(int n){
+		_sem = new Semaphore(1);
+		_n = n;
+		_threads = new Thread[n];
+		for (int i = 0; i< n/2; i++){
+			_threads[i] = new Incrementador(1000, _sem);
+		}
+		for (int i = 0; i<n/2; i++){
+			_threads[i+n/2] = new Decrementador(1000, _sem);
+		}
+	}
+	
+	public void run(){
+	
+		for (int i = 0; i<_n; i++){
+			_threads[i].run();
+		}
+		for (int i = 0; i<_n; i++){
+			try {
+				_threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("All done! Var = " + _var);
+	
+	}
+}
